3a36778d18ab02d29d1c8848da3f9a9db4ca3d4e
Nathan Freeman <nathanlfreeman@gmail.com>
2023-05-24 13:14:42 -0400

    install and test bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Header from './Header/header'
 import Navigation from './Navigation/navigation'
 import BugState from './interfaces/bug'
 import ProjectState from './interfaces/project'
+import { Button } from 'react-bootstrap'
 
 const App: React.FC = () => {
   const [bugs, setBugs] = useState<BugState[]>([])
@@ -58,6 +59,17 @@ const App: React.FC = () => {
       }
 
       {/* End Content */}
+
+      {/* Testing Bootstrap */}
+      <Button variant="primary">Primary</Button>{' '}
+      <Button variant="secondary">Secondary</Button>{' '}
+      <Button variant="success">Success</Button>{' '}
+      <Button variant="warning">Warning</Button>{' '}
+      <Button variant="danger">Danger</Button>{' '}
+      <Button variant="info">Info</Button>{' '}
+      <Button variant="light">Light</Button>{' '}
+      <Button variant="dark">Dark</Button> <Button variant="link">Link</Button>
+
       
     </>
   )
