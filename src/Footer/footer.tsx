d10f021233d3a2931c8b27e09a1a174bc202eb2e
Nathan Freeman <nathanlfreeman@gmail.com>
2023-07-05 14:24:16 -0400

    update footer text

--- src/Footer/footer.tsx
+++ src/Footer/footer.tsx
@@ -10,7 +10,7 @@ const Footer: React.FunctionComponent = () => {
                         <p className="text-dark lead"> a bug tracker by <a className="link-info" href="http://www.nathanfreeman.com" target="_blank">Nathan Freeman</a></p>
                     </div>
                     <div className="col-9 col-lg-4 p-3">
-                        <p className="text-end text-dark footer-script">Built with React, TypeScript, Bootstrap on the front end. Back end TBD but likely Flask running PostgreSQL data storage. Bugnaught is a hypothetical engineering team bug tracking application. It has been designed primarily as a showcase project for software engineer Nathan Freeman.</p>
+                        <p className="text-end text-dark footer-script">Built with React, TypeScript, Bootstrap on the front end. Back end is Flask/Python accessing a MySQL database hosted on PlanetScale. Bugnaught is a hypothetical engineering team bug tracking application. It has been designed primarily as a showcase project for software engineer Nathan Freeman.</p>
                     </div>
                 </div>
             </footer>
